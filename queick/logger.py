9cf276b4ef963ae3aaf069474da0332511f7b603
asmsuechan <suenagaryoutaabc@gmail.com>
2020-11-05 00:46:52 +0900

    Avoid using root logger

--- queick/logger.py
+++ queick/logger.py
@@ -2,11 +2,10 @@ from logging import Formatter, StreamHandler
 from logging import CRITICAL, DEBUG, INFO, ERROR, WARNING
 from logging import addLevelName, getLevelName, getLogger
 
-def setup_logger(loggername=None, loglevel=INFO):
+def setup_logger(loglevel=INFO):
     formatter = Formatter('[%(levelname)s] %(asctime)s - %(message)s')
 
-    _loggername = loggername if loggername else __name__
-    logger = getLogger(loggername)
+    logger = getLogger('queick')
 
     addLevelName(CRITICAL, "\033[1;41m%s\033[1;0m" % getLevelName(CRITICAL))
     addLevelName(INFO , "\033[1;32m%s\033[1;0m" % getLevelName(INFO))
@@ -22,5 +21,3 @@ def setup_logger(loggername=None, loglevel=INFO):
     logger.setLevel(loglevel)
     logger.addHandler(handler)
     logger.propagate = False
-
-    return logger
