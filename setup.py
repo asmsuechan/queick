e247a10073577080aebdd67226be50fa5f5e6728
asmsuechan <suenagaryoutaabc@gmail.com>
2020-11-07 00:17:58 +0900

    Bump to v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="queick",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/asmsuechan/queick",
     description="A lightweight job-queue management system",
     author="asmsuechan",
