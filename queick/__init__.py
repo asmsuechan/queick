da9cbd7aad1bc0d9761df7d04c9276f0c34c03ff
asmsuechan <suenagaryoutaabc@gmail.com>
2020-11-07 12:59:11 +0900

    Fix test

--- queick/__init__.py
+++ queick/__init__.py
@@ -2,3 +2,4 @@ from .worker import Worker
 from .queue_manager import QueueManager
 from .constants import RETRY_TYPE
 from .job_queue import JobQueue
+from .exceptions import *
--- tests/test_job.py
+++ tests/test_job.py
@@ -2,6 +2,7 @@ import unittest
 from concurrent.futures import ThreadPoolExecutor
 
 from queick.job import Job
+from queick.exceptions import NoSuchJobError
 
 class TestJob(unittest.TestCase):
     def setUp(self):
@@ -14,8 +15,9 @@ class TestJob(unittest.TestCase):
         Job('tests.testfunc.func', ('test',), None, None, None)
 
     def test_init_error(self):
-        with self.assertRaises(ModuleNotFoundError):
-            Job('tests.notfound.func', ('test',), None, None, None)
+        with self.assertRaises(NoSuchJobError):
+            j = Job('tests.notfound.func', ('test',), None, None, None)
+            j.func
 
     def test_perform(self):
         executor = ThreadPoolExecutor(max_workers=1)
